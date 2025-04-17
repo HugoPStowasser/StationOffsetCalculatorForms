9789730a06b96660be2ec2c465734fa04fcf8ff9
Hugo Stowasser <hugopstowasser@hotmail.com>
2025-04-16 22:14:04 -0300

    tests: finished tests

--- StationOffsetCalculator.Tests/StationOffsetCalculatorTests.cs
+++ StationOffsetCalculator.Tests/StationOffsetCalculatorTests.cs
@@ -6,7 +6,7 @@ namespace StationOffsetCalculator.Tests
     public class StationOffsetCalculatorTests
     {
         [TestMethod]
-        public void Calculate_PointOnPolyline_ReturnsZeroOffset()
+        public void Calculate_PointOnFirstSegment_ReturnsZeroOffset()
         {
             var points = new List<Point>
             {
@@ -15,7 +15,7 @@ namespace StationOffsetCalculator.Tests
                 new Point(10, 10)
             };
             var polyline = new Polyline(points);
-            var calculator = new StationOffsetCalculator.Core.Services.StationOffsetCalculator();
+            var calculator = new Core.Services.StationOffsetCalculator();
             var targetPoint = new Point(5, 0);
 
             var result = calculator.Calculate(polyline, targetPoint);
@@ -27,7 +27,7 @@ namespace StationOffsetCalculator.Tests
         }
 
         [TestMethod]
-        public void Calculate_PointNotOnPolyline_ReturnsCorrectStationAndOffset()
+        public void Calculate_PointBetweenSegments_ReturnsCorrectOffset()
         {
             var points = new List<Point>
             {
@@ -48,7 +48,7 @@ namespace StationOffsetCalculator.Tests
         }
 
         [TestMethod]
-        public void Calculate_PointNearestToSecondSegment_ReturnsCorrectStationAndOffset()
+        public void Calculate_PointNearestToSecondSegment_ReturnsCorrectValues()
         {
             var points = new List<Point>
             {
@@ -65,6 +65,85 @@ namespace StationOffsetCalculator.Tests
             Assert.AreEqual(15, result.Station, 0.0001);
             Assert.AreEqual(5, result.Offset, 0.0001);
             Assert.AreEqual(10, result.NearestPoint.X, 0.0001);
+            Assert.AreEqual(5, result.NearestPoint.Y, 0.0001);
+        }
+
+        [TestMethod]
+        public void Calculate_DegenerateSegment_HandledGracefully()
+        {
+            var points = new List<Point>
+            {
+                new Point(0, 0),
+                new Point(0, 0),
+                new Point(10, 0)
+            };
+            var polyline = new Polyline(points);
+            var calculator = new Core.Services.StationOffsetCalculator();
+            var targetPoint = new Point(5, 0);
+
+            var result = calculator.Calculate(polyline, targetPoint);
+
+            Assert.AreEqual(5, result.Station, 0.0001);
+            Assert.AreEqual(0, result.Offset, 0.0001);
+        }
+
+        [TestMethod]
+        public void Calculate_PointFarFromPolyline_ReturnsCorrectOffset()
+        {
+            var points = new List<Point>
+            {
+                new Point(0, 0),
+                new Point(0, 10)
+            };
+            var polyline = new Polyline(points);
+            var calculator = new Core.Services.StationOffsetCalculator();
+            var targetPoint = new Point(10, 10);
+
+            var result = calculator.Calculate(polyline, targetPoint);
+
+            Assert.AreEqual(10, result.Station, 0.0001);
+            Assert.AreEqual(10, result.Offset, 0.0001);
+            Assert.AreEqual(0, result.NearestPoint.X, 0.0001);
+            Assert.AreEqual(10, result.NearestPoint.Y, 0.0001);
+        }
+
+        [TestMethod]
+        public void Calculate_PointWithNegativeCoordinates_WorksCorrectly()
+        {
+            var points = new List<Point>
+            {
+                new Point(-10, -10),
+                new Point(0, 0),
+                new Point(10, 0)
+            };
+            var polyline = new Polyline(points);
+            var calculator = new Core.Services.StationOffsetCalculator();
+            var targetPoint = new Point(-5, -5);
+
+            var result = calculator.Calculate(polyline, targetPoint);
+
+            Assert.AreEqual(7.071, result.Station, 0.001);
+            Assert.AreEqual(0, result.Offset, 0.0001);
+        }
+
+        [TestMethod]
+        public void Calculate_PointExactlyOnVertex_ReturnsThatStation()
+        {
+            var points = new List<Point>
+            {
+                new Point(0, 0),
+                new Point(5, 5),
+                new Point(10, 0)
+            };
+            var polyline = new Polyline(points);
+            var calculator = new Core.Services.StationOffsetCalculator();
+            var targetPoint = new Point(5, 5);
+
+            var result = calculator.Calculate(polyline, targetPoint);
+
+            Assert.AreEqual(Math.Sqrt(50), result.Station, 0.0001);
+            Assert.AreEqual(0, result.Offset, 0.0001);
+            Assert.AreEqual(5, result.NearestPoint.X, 0.0001);
             Assert.AreEqual(5, result.NearestPoint.Y, 0.0001);
         }
     }
